ddcdf53c57e6f6dba41dfa848045e1144b85c9b1
fliblip <wickynjy@gmail.com>
2022-11-29 10:36:18 +0300

    Completed assignment

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -10,14 +10,14 @@ class SpicesController < ApplicationController
         render json: spice, status: :created
     end
 
-    # def show
-    #     spice = Spice.find_by(id: params[:id])
-    #     if spice
-    #         render json: spice
-    #     else
-    #         render json: {error: "Spice not found"}, status: :not_found
-    #     end
-    # end
+    def show
+        spice = Spice.find_by(id: params[:id])
+        if spice
+            render json: spice
+        else
+            render json: {error: "Spice not found"}, status: :not_found
+        end
+    end
 
     def update
         spice = Spice.find_by(id: params[:id])
